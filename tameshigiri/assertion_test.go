8e10f7cf9d0d58cd9db62013c16781f3cde76717
Juti Noppornpitak <jnopporn@shiroyuki.com>
2015-09-20 02:28:40 -0400

    Updated the documentation.

--- tameshigiri/assertion_test.go
+++ tameshigiri/assertion_test.go
@@ -2,11 +2,16 @@ package tameshigiri
 
 import "testing"
 
+//
 func ExampleAssertion() {
     var t         = &testing.T{}
     var assertion = Assertion{ T: t }
     var expected  = 123
     var actual    = 123
 
-    assertion.Equals(expected, actual, "Something must be wrong.")
+    assertion.IsTrue(expected == actual, "Ye")
+    assertion.IsFalse(expected != actual, "Ne")
+    assertion.Equals(expected, actual, "Eh?")
+
+    // This example shows all passed assertions.
 }
